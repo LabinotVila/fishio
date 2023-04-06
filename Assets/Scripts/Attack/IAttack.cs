3c895b5a6d57d0e85acd7cad19f39948adc01507
LendritIbrahimi <l.ibrahimi@celonis.com>
2023-04-06 19:13:03 +0200

    reverted some incomplete features

--- Assets/Scripts/Attack/IAttack.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class IAttack : MonoBehaviour
-{
-    public abstract void Attack();
-    public abstract void SetDamage(int damage);
-}
